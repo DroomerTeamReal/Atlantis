0082d5524268544abd5735a6f42a08aa5df59282
DroomerTeam <scpnoob999@gmail.com>
2025-06-22 19:29:15 +0200

    Update ratdex.lua

--- ratdex.lua
+++ ratdex.lua
@@ -1,5 +1,7 @@
+-- Auto-created folders in workspace
+
 -- v1
--- | SPDM | Global definitions
+-- | Atlantis V6 | Global definitions
 
 local arceus = arceus or nil
 local unavailable = function(fake)
@@ -433,7 +435,7 @@ do
 
 	local utils = {};
 
-	function utils:RandomString(len: number) -- SPDM
+	function utils:RandomString(len: number) -- Atlantis V6
 		local chars = {}
 		for i = 1, len or math.random(16, 32) do
 			chars[i] = string.char(math.random(33, 230))
@@ -457,7 +459,7 @@ do
 			end
 		end
 
-		--[[ SPDM | Random name protection, this system is not compatible as it access the instances by the name.
+		--[[ Atlantis V6 | Random name protection, this system is not compatible as it access the instances by the name.
 		
 			if isRunning and not isStudio then
 				instance.Name = utils:RandomString(nil)
@@ -597,7 +599,7 @@ do
 
 	local utils = {};
 	
-	-- SPDM
+	-- Atlantis V6
 	function utils:Notify(text: string)
 		cloneref(game:GetService("StarterGui")):SetCore("SendNotification", {
 			Title = "Atlantis" .. (isiosdevice() and "iOS" or "Android"),
@@ -691,7 +693,7 @@ do
 				return;
 			end
 			
-			pcall(function() -- SPDM
+			pcall(function() -- Atlantis V6
 				data = httpService:JSONDecode(hostData)
 			end)
 		end
@@ -881,8 +883,8 @@ do
 	local signalCache = {};
 	local settingsCache = {
 		executor = {
-			openingMode = "Floating Icon", -- SPDM Team | Floating Icon
-			showParticles = true, -- SPDM Team | Show Particles Setting
+			openingMode = "Floating Icon", -- Atlantis V6 Team | Floating Icon
+			showParticles = true, -- Atlantis V6 Team | Show Particles Setting
 			autoExecute = true,
 			autoSaveTabs = true,
 			fps = {
@@ -2794,7 +2796,7 @@ do
 	end
 
 	local function registerCharacter(character: Instance)
-		-- SPDM | Error prevention checks
+		-- Atlantis V6 | Error prevention checks
 		if not cache then
 			cache = {}
 		end
@@ -2880,7 +2882,7 @@ do
 	local map = {
 		{
 			title = "Executor",
-			items = { -- SPDM Team | Opening Mode Setting
+			items = { -- Atlantis V6 Team | Opening Mode Setting
 				{
 					title = "Opening Mode",
 					linkedSetting = "executor.openingMode",
@@ -2888,7 +2890,7 @@ do
 					items = { "Floating Icon", "Edge Swipe", "Invisible Edge Swipe" },
 					value = "Floating Icon"
 				},
-				{ -- SPDM Team | Show Particles Setting
+				{ -- Atlantis V6 Team | Show Particles Setting
 					title = "Show Floating Icon Particles",
 					linkedSetting = "executor.showParticles",
 					optionType = "toggle",
@@ -3718,7 +3720,7 @@ do
 				popups:Show("dropdown", newDropdown, newDropdown.instance.indicator);
 				newDropdown.selectionChangedConnection = dropdownPopup.onSelectionChanged:Connect(function(value: string)
 					determiningDict[determiningKey] = value;
-					task.delay(0.3, function () popups:Hide("dropdown"); end); -- SPDM Team | Auto-close contextmenu
+					task.delay(0.3, function () popups:Hide("dropdown"); end); -- Atlantis V6 Team | Auto-close contextmenu
 				end);
 			end
 		end);
@@ -4092,7 +4094,7 @@ do
 			IgnoreGuiInset = true,
 			Name = "gui",
 			ResetOnSpawn = false,
-			ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets, -- SPDM Team | Notch-aware UI
+			ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets, -- Atlantis V6 Team | Notch-aware UI
 			ZIndexBehavior = Enum.ZIndexBehavior.Global
 		}, {
 			instanceUtils:Create("Frame", {
@@ -4248,7 +4250,7 @@ do
 					Text = "Premium User?  <font color=\"#eb4545\">Click Here!</font>", 
 					TextColor3 = Color3.fromHex("9fa4ba")
 				}),
-				textButton({ -- SPDM Team | Buy Premium Button
+				textButton({ -- Atlantis V6 Team | Buy Premium Button
 					AnchorPoint = Vector2.new(0.5, 1), 
 					AutomaticSize = Enum.AutomaticSize.None,
 					MouseButton1Click = function()
@@ -4449,7 +4451,7 @@ do
 			Size = UDim2.new(0, 0, 1, 0),
 			ZIndex = 2
 		}, {
-			instanceUtils:Create("TextButton", { -- SPDM Team | Floating Icon
+			instanceUtils:Create("TextButton", { -- Atlantis V6 Team | Floating Icon
 				AnchorPoint = Vector2.new(0.5, 0.5),
 				BackgroundColor3 = Color3.fromHex("15151d"), 
 				BackgroundTransparency = .25,
@@ -4550,7 +4552,7 @@ do
 					FontSize = Enum.FontSize.Size12, 
 					Name = "poweredBy", 
 					Position = UDim2.new(0, 78, 0, 59), 
-					Text = "Powered By SPDM Team", 
+					Text = "Powered By Atlantis V6 Team", 
 					TextColor3 = Color3.fromHex("717176"), 
 					TextSize = 12, 
 					TextTransparency = 1, 
@@ -4575,7 +4577,7 @@ do
 			})
 		});
 		
-		-- SPDM Team | Streak & Expiring Timer
+		-- Atlantis V6 Team | Streak & Expiring Timer
 		local function fetchData()
 			local data = {
 				expiry = 0,
@@ -4647,7 +4649,7 @@ do
 				end
 			end
 
-			-- SPDM Team | Credits
+			-- Atlantis V6 Team | Credits
 			local textLabel = bar.main.poweredBy;
 			local isAnimating = false;
 
@@ -4666,7 +4668,7 @@ do
 				end
 			end
 			
-			local function displayPoweredBySPDM()
+			local function displayPoweredByAtlantis V6()
 				if timeLeft >= 0 and timeLeft <20 then return end; 
 				isAnimating = true
 				local originalText = textLabel.Text
@@ -4676,7 +4678,7 @@ do
 
 				fadeOut:Play()
 				fadeOut.Completed:Wait()
-				textLabel.Text = "Powered By SPDM Team"
+				textLabel.Text = "Powered By Atlantis V6 Team"
 				fadeIn:Play()
 				fadeIn.Completed:Wait()
 
@@ -4710,7 +4712,7 @@ do
 			task.spawn(function()
 				while true do
 					wait(math.random(10, 20))
-					displayPoweredBySPDM()
+					displayPoweredByAtlantis V6()
 				end
 			end)
 		end
@@ -4718,7 +4720,7 @@ do
 		updateText()
 		
 
-		-- SPDM Team | Floating Icon
+		-- Atlantis V6 Team | Floating Icon
 		arceus_libs.input.handleCustomDrag(bar.floatingIcon)
 		arceus_libs.buttons.holdable(bar.floatingIcon).ShortClick.Event:Connect(function()
 			if userSettings.cache.executor.showParticles then
@@ -4880,7 +4882,7 @@ do
 			Value = indent;
 		});
 
-		-- SPDM | Indent pages
+		-- Atlantis V6 | Indent pages
 		if selected and map[selected] then
 			instanceUtils:Tween(map[selected], 0.25, {
 				Position = UDim2.new(0, indent, 1, 0);
@@ -4904,7 +4906,7 @@ do
 			end);
 		end
 
-		-- SPDM Team | Opening modes handler
+		-- Atlantis V6 Team | Opening modes handler
 		local function createTween(target, properties)
 			return game:GetService("TweenService"):Create(target, TweenInfo.new(0.25, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), properties)
 		end
@@ -5745,7 +5747,7 @@ do
 
 	--[[ Functions ]]--
 
-	-- | SPDM | Riky47#3355
+	-- | Atlantis V6 | Riky47#3355
 
 	local debounceTime = 0.1
 	local lastUpdateTime = 0
@@ -5844,7 +5846,7 @@ do
 	end
 
 	local function handleLexResult(lexResult: {any}, addTruncateEllipsis: boolean)
-		--[[ SPDM | You can eventually enable this
+		--[[ Atlantis V6 | You can eventually enable this
 		
 		local currentTime = os.clock()
 		if currentTime - lastUpdateTime < debounceTime then
@@ -6155,4 +6157,4 @@ do
 end
 
 arceus_libs.renderer.startRendering()
-framework.init(); -- SPDM Team
+framework.init(); -- Atlantis V6 Team
